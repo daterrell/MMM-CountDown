51eaedea2f826854eea95232761932dd9a0f0693
Dave Terrell <5710124+daterrell@users.noreply.github.com>
2020-11-21 12:37:51 -0800

    first commit

---
+++ MMM-CountDown.js
@@ -0,0 +1,87 @@
+Module.register("MMM-CountDown", {
+    defaults: {
+        event: "Millenium:",
+        date: "3000-01-01",
+        daysLabel: "days ",
+        allowNegative: false,
+        toTime: false // Whether the countdown is to a specific time
+    },
+
+    updateFreq: "CLOCK_MINUTE",
+
+    templateData: {
+        event: null,
+        time: null,
+        label: null
+    },
+
+    getHeader: function () {
+        return false;
+    },
+
+    getScripts: function () {
+        return ["moment.js", "moment-timezone.js"];
+    },
+
+    getStyles: function () {
+        return ["MMM-CountDown.css"];
+    },
+
+    getTemplate: function () {
+        return "MMM-CountDown.njk"
+    },
+
+    getTemplateData: function () {
+        return this.templateData;
+    },
+
+    // set update interval
+    start: function () {
+        console.log("Starting up " + this.name);
+        this.updateData();
+        this.updateDom();
+    },
+
+    notificationReceived: function (notification) {
+        if (notification === this.updateFreq) {
+            this.updateData();
+            this.updateDom();
+        }
+    },
+
+    updateData: function () {
+        var target = moment(this.config.date).add(1, "day");
+        var now = moment();
+        var timeDiff = target.diff(now);
+        var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+        if (Math.abs(diffDays) > 1) {
+            updateTemplateData(Math.abs(diffDays), this.config.daysLabel);
+
+            if (diffDays <= -1 && !this.config.allowNegative) {
+                this.hide();
+                return null;
+            }
+        } else if (diffDays < 0 && diffHours < 24 && !this.config.toTime) {
+            updateTemplateData("TODAY!", null);
+        } else {
+            this.updateFreq = "CLOCK_SECOND";
+            var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+            var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+            updateTemplateData(`T-${this.pad(diffHours, 2)}:${this.pad(diffMinutes, 2)}:${this.pad(diffSeconds, 2)}`, null);
+        }
+    },
+
+    updateTemplateData(date, label) {
+        this.templateData.date = date;
+        this.templateData.label = label;
+        return this.templateData;
+    },
+
+    pad: function pad(num, size) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num;
+    }
+});
---
+++ README.md
@@ -0,0 +1,35 @@
+# MMM-CountDown
+![Screenshot](https://github.com/daterrell/MMM-CountDown/raw/master/screenshots/screenshot.png)
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) which can count down the days to a date/event, or up from a date/time
+
+## Why?
+Yes, there's another CountDown module, and again I didn't like the style/output, and wanted to have a few customizations that I didn't think fit with [the original](https://github.com/boazarad/MMM-CountDown) by [Boaz](https://github.com/boazarad)
+
+## How?
+### Manual install
+
+1. Clone this repository in your `modules` folder:
+  ```bash
+  cd ~/MagicMirror/modules # adapt directory if you are using a different one
+  git clone https://github.com/daterrell/MMM-CountDown.git
+  cd MMM-CountDown
+  ```
+2. Add the module to your `config/config.js` file.
+  ```js
+  {
+    module: 'MMM-CountDown',
+    position: 'bottom_center',
+    config: {
+        // See configuration options
+    }
+  },
+  ```
+
+## Configuration options
+
+| Option           | Description                                                                                                           |
+| ---------------- | --------------------------------------------------------------------------------------------------------------------- |
+| `position`       | *Required* Where do you want to place the counter (use standard magicmirror positions)                                |
+| `event`          | *Required* Name of event to count down to (displayed above counter)                                                   |
+| `date`           | *Required* Date to count down to (YYYY-MM-DD HH:MM:SS)                                                                |
