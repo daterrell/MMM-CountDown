708fc6e6b3d0a19dda9a312cef72cf509f643104
Dave Terrell <5710124+daterrell@users.noreply.github.com>
2020-11-22 14:45:31 -0800

    Fix negative toTime configurations

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -50,19 +50,27 @@ Module.register("MMM-CountDown", {
     },
 
     updateData: function () {
-        var target = moment(this.config.date).add(1, "day");
+        var target = moment(this.config.date);
         var now = moment();
         var timeDiff = target.diff(now);
-        var diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        var diffDays = target.diff(now, "days");
+        var diffHours = target.diff(now, "hours");
 
-        if (Math.abs(diffDays) > 1) {
-            this.updateTemplateData(Math.abs(diffDays), this.config.daysLabel);
+        if (target.isBefore(now) &&
+            this.config.allowNegative === false) {
+            this.hide();
+            return null;
+        }
 
-            if (diffDays <= -1 && !this.config.allowNegative) {
-                this.hide();
-                return null;
-            }
-        } else if (diffDays < 0 && diffHours < 24 && !this.config.toTime) {
+        if (this.config.allowNegative === true) diffDays = Math.abs(diffDays);
+
+        // Hours 0-23:59:59 are counted as zero.  Add a day to accomodate.
+        if (this.config.toTime === false) diffDays += 1;
+
+        if (diffDays >= 1) {
+            this.updateTemplateData(diffDays, this.config.daysLabel);
+        } else if (this.config.toTime === false &&
+            diffDays < 0 && diffHours < 24) {
             this.updateTemplateData("TODAY!", null);
         } else {
             this.updateFreq = "CLOCK_SECOND";
