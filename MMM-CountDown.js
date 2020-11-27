7c7d891f867c5b16e8867534ad3a5977b91d23e0
Dave Terrell <5710124+daterrell@users.noreply.github.com>
2020-11-27 00:11:29 -0800

    Rewrite with classes.  Hide/disable if date past

--- MMM-CountDown.js
+++ MMM-CountDown.js
@@ -1,3 +1,72 @@
+class TimeDiff {
+    static millisPerSecond = 1000;
+    static millisPerMinute = TimeDiff.millisPerSecond * 60;
+    static millisPerHour = TimeDiff.millisPerMinute * 60;
+    static millisPerDay = TimeDiff.millisPerHour * 24;
+
+    diffDays;
+    diffHours;
+    diffMinutes;
+    diffSeconds;
+    isPast;
+
+    constructor(date) {
+        this.target = moment(date);
+        this.update();
+    }
+
+    update() {
+        let now = moment();
+        let timeDiff = Math.abs(this.target.diff(now));
+
+        this.diffDays = Math.floor(timeDiff / TimeDiff.millisPerDay);
+        this.diffHours = Math.floor(timeDiff / TimeDiff.millisPerHour);
+        this.diffMinutes = Math.floor(timeDiff / TimeDiff.millisPerMinute);
+        this.diffSeconds = Math.floor(timeDiff / TimeDiff.millisPerSecond);
+        this.isPast = this.target.isBefore(now);
+    }
+}
+
+class TemplateData {
+    timeDiff;
+    isToTime;
+
+    event;
+    date;
+    label;
+
+    constructor(event, timeDiff, isToTime) {
+        this.event = event;
+        this.timeDiff = timeDiff;
+        this.isToTime = isToTime;
+    }
+
+    update() {
+        this.timeDiff.update();
+        this.updateData();
+    }
+
+    updateData() {
+        if (this.timeDiff.diffDays >= 1) {
+            this.date = this.timeDiff.diffDays;
+            if (!this.timeDiff.isPast) this.diffDays += 1;
+        } else if (this.timeDiff.diffDays === 0 &&
+            this.isToTime === false) {
+            this.date = "TODAY!";
+            this.label = null;
+        } else {
+            this.date = `T-${this.pad(timeDiff.diffHours, 2)}:${this.pad(timeDiff.diffMinutes, 2)}:${this.pad(timeDiff.diffSeconds, 2)}`;
+            this.label = null;
+        }
+    }
+
+    pad(num, size) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num;
+    }
+}
+
 Module.register("MMM-CountDown", {
     defaults: {
         event: "Millenium:",
@@ -7,13 +76,9 @@ Module.register("MMM-CountDown", {
         toTime: false // Whether the countdown is to a specific time
     },
 
-    updateFreq: "CLOCK_MINUTE",
+    templateData: null,
 
-    templateData: {
-        event: null,
-        time: null,
-        label: null
-    },
+    timeDiff: null,
 
     getHeader: function () {
         return false;
@@ -32,65 +97,37 @@ Module.register("MMM-CountDown", {
     },
 
     getTemplateData: function () {
+        this.templateData.update();
         return this.templateData;
     },
 
     // set update interval
     start: function () {
         console.log("Starting up " + this.name);
-        this.updateData();
-        this.updateDom();
+        this.timeDiff = new TimeDiff(this.config.date);
+        this.templateData = new TemplateData(this.config.event, this.timeDiff, this.config.toTime);
+        this.update();
     },
 
     notificationReceived: function (notification) {
-        if (notification === this.updateFreq) {
-            this.updateData();
-            this.updateDom();
+        if (notification === "CLOCK_SECOND") {
+            this.update();
         }
     },
 
-    updateData: function () {
-        var target = moment(this.config.date);
-        var now = moment();
-        var timeDiff = target.diff(now);
-        var diffDays = target.diff(now, "days");
-        var diffHours = target.diff(now, "hours");
-
-        if (target.isBefore(now) &&
-            this.config.allowNegative === false) {
-            this.hide();
-            return null;
-        }
-
-        if (this.config.allowNegative === true) diffDays = Math.abs(diffDays);
-
-        // Hours 0-23:59:59 are counted as zero.  Add a day to accomodate.
-        if (this.config.toTime === false) diffDays += 1;
-
-        if (diffDays >= 1) {
-            this.updateTemplateData(diffDays, this.config.daysLabel);
-        } else if (this.config.toTime === false &&
-            diffDays < 0 && diffHours < 24) {
-            this.updateTemplateData("TODAY!", null);
-        } else {
-            this.updateFreq = "CLOCK_SECOND";
-            var diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-            var diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-            this.updateTemplateData(`T-${this.pad(diffHours, 2)}:${this.pad(diffMinutes, 2)}:${this.pad(diffSeconds, 2)}`, null);
-        }
+    update: function () {
+        if (this.shouldHide()) this.disable();
+        else this.updateDom();
     },
 
-    updateTemplateData(date, label) {
-        this.templateData.date = date;
-        this.templateData.label = label;
-        this.templateData.event = this.config.event;
-        return this.templateData;
+    shouldHide: function () {
+        return this.config.allowNegative === false &&
+            this.timeDiff.isPast &&
+            this.timeDiff.diffHours >= 24;
     },
 
-    pad: function pad(num, size) {
-        num = num.toString();
-        while (num.length < size) num = "0" + num;
-        return num;
+    disable: function () {
+        this.notificationReceived = function () { };
+        this.hide();
     }
 });
